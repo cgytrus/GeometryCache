4d890efc55a7a56e6d46a8e8cc020d321c84619a
ConfiG <cgytrus@cgyt.ru>
2022-10-08 15:38:13 +0300

    switch to qoixx

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Caches textures to QOI for faster startup
 
 Hasn't been tested extensively yet but on my setup it increases loading time by 1 second (from 3 to 2)
+
+Uses [wx257osn2/qoixx](https://github.com/wx257osn2/qoixx) for QOI encoding/decoding
--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,30 +1,38 @@
+#include "qoixx.hpp"
+
 #include "includes.h"
 #include <filesystem>
 #include <iostream>
 #include <fstream>
-#include "qoi/QOI.hpp"
 
 const CCImage::EImageFormat kFmtQoi = (CCImage::EImageFormat)69;
 
 void cacheImage(CCImage* self, std::filesystem::file_time_type qoiWriteTime, std::filesystem::path qoiPath) {
     auto m_nWidth = *(unsigned short*)((uintptr_t)self + 0x20);
     auto m_nHeight = *(unsigned short*)((uintptr_t)self + 0x22);
-    auto m_pData = *(const uint8_t**)((uintptr_t)self + 0x28);
+    auto m_pData = *(unsigned char**)((uintptr_t)self + 0x28);
     auto m_bHasAlpha = *(bool*)((uintptr_t)self + 0x2c);
     auto m_bPreMulti = *(bool*)((uintptr_t)self + 0x2d);
 
-    QOIEncoder encoder;
+    qoixx::qoi::desc desc {
+        .width = m_nWidth,
+        .height = m_nHeight,
+        .channels = m_bHasAlpha ? (uint8_t)4u : (uint8_t)3u,
+        .colorspace = m_bPreMulti ? qoixx::qoi::colorspace::linear : qoixx::qoi::colorspace::srgb
+    };
 
-    if(!encoder.encode(m_nWidth, m_nHeight, m_pData, m_bHasAlpha, m_bPreMulti))
-        return;
+    try {
+        auto encoded = qoixx::qoi::encode<std::vector<char>>(m_pData, desc.width * desc.height * desc.channels, desc);
 
-    std::ofstream qoiFile;
-    qoiFile.open(qoiPath, std::ios::out | std::ios::trunc | std::ios::binary);
-    qoiFile.write((const char*)encoder.getEncoded(), encoder.getEncodedSize());
-    qoiFile.close();
+        std::ofstream qoiFile;
+        qoiFile.open(qoiPath, std::ios::out | std::ios::trunc | std::ios::binary);
+        qoiFile.write(encoded.data(), encoded.size());
+        qoiFile.close();
 
-    // used instead of actual hash for better performance
-    std::filesystem::last_write_time(qoiPath, qoiWriteTime);
+        // used instead of actual hash for better performance
+        std::filesystem::last_write_time(qoiPath, qoiWriteTime);
+    }
+    catch(...) { }
 }
 
 bool (__thiscall* CCImage_initWithImageFile)(CCImage*, const char*, CCImage::EImageFormat);
@@ -56,21 +64,28 @@ bool __fastcall CCImage_initWithImageFileThreadSafe_H(CCImage* self, void*, cons
 }
 
 bool initWithQoiData(CCImage* self, void* pData, int nDataLen) {
-    QOIDecoder decoder;
+    try {
+        auto decoded = qoixx::qoi::decode<std::vector<unsigned char>>((unsigned char*)pData, nDataLen);
 
-    if(!decoder.decode((const uint8_t*)pData, nDataLen))
+        *(unsigned short*)((uintptr_t)self + 0x20) = decoded.second.width;
+        *(unsigned short*)((uintptr_t)self + 0x22) = decoded.second.height;
+        *(int*)((uintptr_t)self + 0x24) = 8; // m_nBitsPerComponent
+        *(bool*)((uintptr_t)self + 0x2c) = decoded.second.channels >= 4u;
+        // actually is premultiplied alpha but the linear colorspace field is not used so why not use it for this instead
+        // this may make some (or all) images display weirdly in proper qoi viewers tho
+        *(bool*)((uintptr_t)self + 0x2d) = decoded.second.colorspace == qoixx::qoi::colorspace::linear;
+
+        // copy decoded image to CCImage
+        auto m_pData = (unsigned char**)((uintptr_t)self + 0x28);
+        *m_pData = new unsigned char[decoded.first.size()];
+        for(size_t i = 0; i < decoded.first.size(); ++i)
+            (*m_pData)[i] = decoded.first[i];
+
+        return true;
+    }
+    catch(...) {
         return false;
-
-    *(unsigned short*)((uintptr_t)self + 0x20) = decoder.getWidth();
-    *(unsigned short*)((uintptr_t)self + 0x22) = decoder.getHeight();
-    *(int*)((uintptr_t)self + 0x24) = 8; // m_nBitsPerComponent
-    *(unsigned char**)((uintptr_t)self + 0x28) = (unsigned char*)decoder.getPixels();
-    *(bool*)((uintptr_t)self + 0x2c) = decoder.hasAlpha();
-    // actually is premultiplied alpha but the linear colorspace field is not used so why not use it for this instead
-    // this may make some (or all) images display weirdly in proper qoi viewers tho
-    *(bool*)((uintptr_t)self + 0x2d) = decoder.isLinearColorspace();
-
-    return true;
+    }
 }
 
 bool (__thiscall* CCImage_initWithImageData)(CCImage*, void*, int, CCImage::EImageFormat, int, int, int);
